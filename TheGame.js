3fc9055beda74ccad1e9fb9508f93fe315e3e66a
Paul Perez <pperez@codeclubs.com>
2019-04-14 23:38:00 -0600

    Modified   TheGame.css Modified   TheGame.htm Modified   TheGame.js Add        res/CodeClubsSQ.png Add        resources.js
    Adds state machine and rules engine. Adds supplemental resources to complete the programming.

--- TheGame.js
+++ TheGame.js
@@ -1,19 +1,21 @@
-(function(ns){
-    let filename = 'file://G:/Team Drives/CodeClubs.org/Administration/CodeClubs.png';
+window['NS'] = window['NS'] || {};
+(function(NS) {
+    NS.res = NS['res'] || {};
     class Card {
-        constructor(img) {
-            this.img = img;
+        constructor(icon) {
+            this.icon = icon;
         }
-        generateHTML() {
-            return '<span><img src="'+this.img+'"></span>'
-        }
-        
+    
         render() {	
 
             let scene = document.createElement('div');
             let card = document.createElement('div');
             let front = document.createElement('div');
             let back = document.createElement('div');
+            let icon = document.createElement('i');
+
+            icon.classList.add('fa')
+            icon.classList.add(this.icon);
 
             scene.classList.add('scene');
             card.classList.add('card');
@@ -21,33 +23,66 @@
             back.classList.add('face');
             front.classList.add('front');
             back.classList.add('back');
-
-            //let i = document.createElement('img');
-            //i.src = filename
+            
             
             scene.appendChild(card);
+            back.appendChild(icon)
             card.appendChild(front);
             card.appendChild(back);
-            //back.appendChild(i);
 
-            card.onclick = function(e) { 
-              if (this.classList.contains('flip')) {
-                  this.classList.remove('flip');
-              } else {
-                  this.classList.add('flip');
-              }
-            }
+            card.onclick = this.flip
             return scene
         }
+        flip(e)  { 
+            let state = NS.game.state;
+            if (this.classList.contains('matched')) return;
+            if (state.second) return;
+            if (state.first == this) return;
+            if (this.classList.contains('flip')) {
+                this.classList.remove('flip');
+            } else {
+                this.classList.add('flip');
+            }
+            if (state.first) {
+                state.second = this;
+                if (state.first.innerHTML == state.second.innerHTML) {
+                    setTimeout(() => {
+                        state.first.classList.add('matched');
+                        state.second.classList.add('matched');
+                        delete state.first;
+                        delete state.second;
+                    }, 1500);
+
+                } else {
+                    setTimeout(() => {
+                        state.first.classList.remove('flip');
+                        state.second.classList.remove('flip');
+                        delete state.first;
+                        delete state.second;
+                    }, 1500);
+                }
+            } else {
+                state.first = this;
+            }
+          }
     }
 
-    class Canvas {
+    class Game {
         constructor(className) {
-           this.canvas = document.getElementsByClassName(className)[0];
-           this.cards = [];
-           for(let i=1;i<=48;i++){
-               this.cards.push(new Card(filename));
-           }        
+            this.canvas = document.getElementsByClassName(className)[0];
+            this.cards = [];
+            this.state = {};
+            this.icons = [];
+            for(let i=1;i<=24;i++){
+                let icon = NS.res.fa()
+                this.icons.push(icon);
+                this.icons.push(icon);
+            }     
+            NS.res.shuffle(this.icons);
+            for(let i=0;i<48;i++){
+                this.cards.push(new Card(this.icons[i], this));
+            }     
+            this.render();   
         }
 
         render(){
@@ -56,22 +91,9 @@
                 this.canvas.appendChild(card.render());
             }
         }
-        refresh(){}
-        layout(){}
-        paint(){}
-        onResize(){}
     }
- 
-    
-    class Game {
-        constructor(className) {
-	  this.canvas = new Canvas(className);
-          this.canvas.render();
-        }
-    }
-    g = new Game('card-table');
-    
-    console.log(g);
 
-   
-})({});
+    NS.game = new Game('card-table');
+
+    
+})(NS || {});
