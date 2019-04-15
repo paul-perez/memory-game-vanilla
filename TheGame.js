9687bc199896c809a291f0a64dbb7e4d43d417cf
Paul Perez <pperez@codeclubs.com>
2019-04-15 10:26:25 -0600

    Modified   TheGame.css Modified   TheGame.htm Modified   TheGame.js
    Adds header, reset and new game buttons, as well as score.

--- TheGame.js
+++ TheGame.js
@@ -1,11 +1,15 @@
 window['NS'] = window['NS'] || {};
 (function(NS) {
-    NS.res = NS['res'] || {};
     class Card {
         constructor(icon) {
             this.icon = icon;
         }
-    
+        destroy() {
+            this.card.removeEventListener("click", this.flip);
+            if (this.container.parentNode) {
+                this.container.parentNode.removeChild(this.container);
+            }
+        }
         render() {	
 
             let scene = document.createElement('div');
@@ -29,8 +33,9 @@ window['NS'] = window['NS'] || {};
             back.appendChild(icon)
             card.appendChild(front);
             card.appendChild(back);
-
-            card.onclick = this.flip
+            card.addEventListener("click", this.flip)
+            this.card = card
+            this.container = scene
             return scene
         }
         flip(e)  { 
@@ -46,13 +51,15 @@ window['NS'] = window['NS'] || {};
             if (state.first) {
                 state.second = this;
                 if (state.first.innerHTML == state.second.innerHTML) {
+                    NS.game.score();
                     setTimeout(() => {
+                        state.first.classList.remove('flip');
+                        state.second.classList.remove('flip');
                         state.first.classList.add('matched');
                         state.second.classList.add('matched');
                         delete state.first;
                         delete state.second;
-                    }, 1500);
-
+                    }, 1000);
                 } else {
                     setTimeout(() => {
                         state.first.classList.remove('flip');
@@ -73,27 +80,67 @@ window['NS'] = window['NS'] || {};
             this.cards = [];
             this.state = {};
             this.icons = [];
+
             for(let i=1;i<=24;i++){
                 let icon = NS.res.fa()
                 this.icons.push(icon);
                 this.icons.push(icon);
             }     
-            NS.res.shuffle(this.icons);
+            //NS.res.shuffle(this.icons);
             for(let i=0;i<48;i++){
                 this.cards.push(new Card(this.icons[i], this));
             }     
             this.render();   
         }
 
-        render(){
+        render() {
             for (let idx in this.cards){
                 let card = this.cards[idx];
                 this.canvas.appendChild(card.render());
+            }
+        }
+
+        destroy() {
+            this.canvas.innerHTML = ''; 
+            for (let idx in this.cards){
+                let card = this.cards[idx];
+                card.destroy();
+             } 
+        }
+
+        clear() {
+            this.canvas.innerHTML = ''; 
+        }
+
+        new_game() {
+            NS.newGame();
+        }
+
+        reset() {
+            NS.game.clear();
+            NS.game.score(true);
+            NS.game.render()
+        }
+
+        score(reset) {
+            let score = document.getElementsByClassName('score')[0];
+            if (reset) {
+                score.innerHTML = '0';
+            } else {
+                console.log(score);
+                score.innerHTML = parseInt(score.innerHTML) + 1;
             }
         }
     }
 
     NS.game = new Game('card-table');
 
+    NS.newGame = function() {
+        if (NS.game) {
+            NS.game.destroy();
+            NS.game.score(true);
+        }
+        NS.game = new Game('card-table');
+    }
     
 })(NS || {});
